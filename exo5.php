0c7d59bceb921e836d3ed6affad786895edce795
BadrMorad <moradpro989@gmail.com>
2024-10-18 16:53:01 +0200

    php

--- exo5.php
+++ exo5.php
@@ -3,11 +3,16 @@
 <h2>Résultat</h2>
 
 <?php
+// Fonction pour afficher le formulaire avec des champs de texte 
 function afficherInput($nomsInput) {
-    $result = '<from>';
+    
+    $result = '<from>'; // Commence le formulaire 
+
     foreach ($nomsInput as $nom) {
+        // Crée une étiquette pour chaque champs de texte 
         $result .= '<label>' . htmlspecialchars($nom) . ':</label><br>';
-        $result .= '<input type="texte" name="' . strtolower(str_replace(' ', '_', $nom)) . '" placeholder="' .htmlspecialchars($nom) . '" ><br>';
+        //Crée un champs de texte avce un nom formaté e minuscule et espaces remplacés par des underscores
+        $result .= '<input type="texte" name="' . strtolower(str_replace(' ', '_', $nom)) . '" placeholder="'  . '" ><br>';
     }
     $result .= '<input type="submit" value=">';
     $result .= '</from>';
